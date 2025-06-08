fb36d7fd46f437002cc3014059f939d5ac2760f4
Martin Amaro <martingadpacks@gmail.com>
2025-06-07 21:10:18 -0500

    feat: add delete user option

--- src/pages/Dashboard/DashboardArticles.jsx
+++ src/pages/Dashboard/DashboardArticles.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { ComingSoon } from '../../components/dashboard/ComingSoon'
+import { DashHeader } from './../../components/dashboard/DashHeader';
+import { DashTitle } from './../../components/dashboard/DashTitle';
 
 export const DashboardArticles = () => {
   return (
-    <ComingSoon />
+    <div className='relative overflow-hidden'>
+      <DashHeader title="Artículos" />
+
+
+      <div className="p-8 max-w-2xl">
+        <DashTitle title="Agrega artículos y servicios">
+          Aquí puedes configurar las opciones de tu panel de control.
+        </DashTitle>
+      </div>
+    </div>
   )
 }
--- src/pages/Dashboard/DashboardSettings.jsx
+++ src/pages/Dashboard/DashboardSettings.jsx
@@ -8,9 +8,12 @@ import { AuthCheck } from '../../components/auth/AuthCheck';
 import { TopMessage } from '../../components/dashboard/TopMessage';
 import { DashHeader } from './../../components/dashboard/DashHeader';
 import { DashTitle } from '../../components/dashboard/DashTitle';
+import { Dialog } from './../../components/dialogs/Dialog';
+import { useNavigate } from 'react-router-dom';
 
 export const DashboardSettings = () => {
   const { user, setUser, login } = useAuth();
+  const navigate = useNavigate();
   const [email, setEmail] = useState(user.email);
   const [newEmail, setNewEmail] = useState(email);
 
@@ -21,10 +24,6 @@ export const DashboardSettings = () => {
   const [success, setSuccess] = useState('');
   const [loading, setLoading] = useState(false);
   const [editMode, setEditMode] = useState('');
-
-  
-
-  console.log(user)
 
   const handleSave = async () => {
     setError('');
@@ -40,7 +39,6 @@ export const DashboardSettings = () => {
             email: newEmail
           });
 
-          // Si el backend responde con el objeto actualizado
           if (response.status === 200) {
             const { user: updatedUser, token: newToken } = response.data;
             localStorage.setItem("token", newToken);
@@ -76,7 +74,7 @@ export const DashboardSettings = () => {
       console.error(err);
 
       if (err.response?.data) {
-        setError(err.response.data); // Si el backend envía un mensaje claro, ej: "El email ya está en uso"
+        setError(err.response.data); 
       } else {
         setError('Ocurrió un error al actualizar los datos');
       }
@@ -84,7 +82,6 @@ export const DashboardSettings = () => {
 
     setLoading(false);
   };
-
 
   const handleCancel = () => {
     setEditMode('');
@@ -95,6 +92,40 @@ export const DashboardSettings = () => {
     setConfirmPassword('');
   };
 
+  const handleDeleteAccount = async () => {
+    return (
+          <Dialog
+              title="Eliminar cuenta"
+              onSave={handleDeleteAccount}
+              onCancel={() => { return "true" }}
+              triggerText='Eliminar'
+              triggerClass='btn-text text-red-500!'
+              canSave={false}
+            >
+              <p className='text-tiny text-neutral-800'>¿Estás seguro de eliminar la cuenta? Esta acción no se puede deshacer.</p>
+            </Dialog>
+        )
+    try {
+      const response = await axios.delete(`${config.backend}/users/${user.id}`, {
+        id: user.id
+      });
+
+      if (response.status === 204) {
+        
+        setSuccess('Se ha eliminado correctamente');
+        navigate("/logout")
+      }
+    }
+    catch (err) {
+      if (err.response?.data) {
+        setError(err.response.data); 
+      } else {
+        setError('Ocurrió un error al actualizar los datos');
+      }
+    }
+
+  }
+
   return (
     <div className='relative overflow-hidden'>
       <DashHeader title="Cuenta" />
@@ -102,7 +133,7 @@ export const DashboardSettings = () => {
       {/* <TopMessage message={success} /> */}
       {success && <TopMessage message={success} type="success" onClose={() => setSuccess('')} />}
       {error && <TopMessage message={error} type="error" onClose={() => setError('')} />}
-      
+
 
       <div className="p-8 max-w-2xl">
         <DashTitle title="Ajustes">
@@ -189,6 +220,23 @@ export const DashboardSettings = () => {
             </div>
           )}
         </div>
+
+        <div className="mt-6 border-b border-gray-200 pb-4 flex flex-col middle:flex-row middle:items-center justify-between">
+          <label className="font-medium text-base text-slate-800 mb-1 block">Mi cuenta</label>
+          <div className="mt-4 middle:mt-0 middle:ml-4 whitespace-nowrap gap-4 flex">
+            <Dialog
+              title="Eliminar cuenta"
+              onSave={handleDeleteAccount}
+              onCancel={() => { return "true" }}
+              triggerText='Eliminar'
+              triggerClass='btn-text text-red-500!'
+              canSave={false}
+            >
+              <p className='text-tiny text-neutral-800'>¿Estás seguro de eliminar la cuenta? Esta acción no se puede deshacer.</p>
+            </Dialog>
+          </div>
+        </div>
+
       </div>
     </div>
   );
