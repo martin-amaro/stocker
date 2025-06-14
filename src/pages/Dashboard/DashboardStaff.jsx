2e1a46cb395963b5176efeb4a72a80d3506dc56a
Martin Amaro <martingadpacks@gmail.com>
2025-06-13 21:57:15 -0500

    feat: add new features to Staff section

--- src/pages/Dashboard/DashboardStaff.jsx
+++ src/pages/Dashboard/DashboardStaff.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { DashHeader } from '../../components/dashboard/DashHeader'
 import { DashTitle } from '../../components/dashboard/DashTitle'
 import { ButtonMain } from '../../components/gui/ButtonMain'
-import { Testl } from './Testl';
-import { Plus } from 'lucide-react';
+import { IdCard, Plus, UsersRound } from 'lucide-react';
 import { Dialog } from '../../components/dialogs/Dialog';
 import { SimpleInput } from '../../components/dashboard/SimpleInput';
 import { GuiSelect } from '../../components/gui/GuiSelect';
@@ -13,36 +12,36 @@ import { useAuth } from '../../context/AuthContext';
 import axios from 'axios';
 import config from '../../config';
 import { LoadingSection } from './../../components/LoadingSection';
+import { isValidEmail } from '../../utils/validators';
+import { TopMessage } from '../../components/dashboard/TopMessage';
+
+
+const RoleType = {
+  'GUEST': ['Invitado', 'Acceso limitado, solo lectura o revisión parcial del sistema.'],
+  'USER': ['Usuario', 'Ejecuta tareas asignadas como entradas, salidas y movimientos.'],
+  'MOD': ['Moderador', 'Supervisa y gestiona contenidos, acciones básicas de usuarios.'],
+  'ADMIN': ['Administrador', 'Control total del sistema, usuarios, configuraciones y permisos.'],
+}
+
 
 export const DashboardStaff = () => {
 
   const { token, user } = useAuth();
   const [staff, setStaff] = useState([]);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  // User data
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('USER');
   const [hash, setHash] = useState('');
+
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [selectedUsers, setSelectedUsers] = useState([]);
+  const [actionLoading, setActionLoading] = useState(false);
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
-
-  // Este array luego puede venir de una API
-  const users = [
-    {
-      id: 1,
-      name: 'Neil Sims',
-      email: 'neil.sims@flowbite.com',
-      position: 'React Developer',
-      status: 'Online',
-      avatar: 'https://i.pinimg.com/474x/2b/da/51/2bda51ca60cc3b5daaa8e062eb880430.jpg',
-    },
-    {
-      id: 2,
-      name: 'Bonnie Green',
-      email: 'bonnie@flowbite.com',
-      position: 'Designer',
-      status: 'Online',
-      avatar: 'https://i.pinimg.com/474x/07/c4/72/07c4720d19a9e9edad9d0e939eca304a.jpg',
-    },
-  ];
 
 
   useEffect(() => {
@@ -58,6 +57,50 @@ export const DashboardStaff = () => {
       })
   }, []);
 
+
+
+  const handleSave = async () => {
+    setActionLoading(true);
+
+    try {
+      const response = await axios.post(`${config.backend}/business/staff/create`, {
+        name: name,
+        email: email,
+        password: hash,
+        role: role
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      setStaff([...staff, response.data])
+      setSuccess('Usuario creado correctamente.');
+
+    }
+    catch (err) {
+      const errorMsg = err.response?.data || err.message;
+      if (errorMsg) {
+        setError(errorMsg);
+      } else {
+        setError('Ocurrió un error al actualizar los datos');
+      }
+    }
+    finally {
+      setActionLoading(false);
+    }
+  }
+
+  const handleCancel = () => {
+    setName("");
+    setEmail("");
+    setRole("USER");
+  }
+
+  const handleSubmit = async () => {
+
+  }
+
   const handleToggleUser = (userId) => {
     setSelectedUsers(prev =>
       prev.includes(userId)
@@ -68,28 +111,15 @@ export const DashboardStaff = () => {
 
   const isSelected = (userId) => selectedUsers.includes(userId);
 
-  const handleSave = () => {
+  const validateInputs = () => {
+    if (!name.trim()) return "El nombre es obligatorio"
+    if (name.length < 3) return "El nombre debe tener al menos 3 caracteres."
 
-  }
+    if (!email.trim()) return "El correo eléctronico es obligatorio";
+    if (!isValidEmail(email)) return "Correo eléctrónico inválido.";
 
-  const handleSubmit = async () => {
-    console.log(user)
-    try {
-      const response = await axios.post(`${config.backend}/users/staff`, {
-        name: 'Perro',
-        email: 'perro@gmail.com',
-        password: '121211'
-      }, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-
-      console.log('Usuario creado:', response.data);
-      // Opcional: redireccionar o mostrar mensaje
-    } catch (error) {
-      console.error('Error creando usuario:', error.response?.data || error.message);
-    }
+    if (!role) return "El rol es obligatorio";
+    return false;
   }
 
 
@@ -103,149 +133,168 @@ export const DashboardStaff = () => {
           Aquí puedes configurar las opciones de tu panel de control.
         </DashTitle> */}
 
+          {success && <TopMessage message={success} type="success" onClose={() => setSuccess('')} />}
+          {error && <TopMessage message={error} type="error" onClose={() => setError('')} />}
+
           <div>
             <div className='flex justify-between items-center py-4'>
               <h3 className='text-xl font-semibold'>Personal</h3>
 
-              <button onClick={handleSubmit}>Enviar</button>
               <Dialog
                 title="Agregar nuevo empleado"
                 onSave={handleSave}
-                onCancel={() => { }}
+                onCancel={handleCancel}
                 triggerText='Agregar nuevo'
                 triggerClass='btn-main flex text-tiny! gap-2'
                 triggerIcon={<Plus />}
                 triggerClick={() => setHash(generatePassword(6))}
-                canSave={false}
+                canSave={validateInputs()}
+                loading={actionLoading}
               >
-                <SimpleInput
-                  label="Nombre"
-                  placeholder="Ingresa el nombre"
-                  className="mb-4"
-                // value={newAddress}
-                // onChange={(e) => setNewAddress(e.target.value)}
-                />
+                <div className='mb-6'>
+                  <SimpleInput
+                    label="Nombre"
+                    placeholder="Ingresa el nombre"
+                    className="mb-4"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
 
-                <SimpleInput
-                  label="Correo electrónico"
-                  placeholder="Ingresa tu nueva dirección"
-                  className="mb-4"
-                // value={newAddress}
-                // onChange={(e) => setNewAddress(e.target.value)}
-                />
+                <div className='mb-6'>
+                  <SimpleInput
+                    label="Correo electrónico"
+                    placeholder="Ingresa tu nueva dirección"
+                    className="mb-4"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
 
-                <label className="font-medium text-base text-slate-800 mb-1 block">Rol</label>
-                <GuiSelect
-                  placeholder='Selecciona un tipo'
-                  value=""
-                  onChange={(e) => setNewBusinessType(e)}
-                  className="mb-4"
-                >
-                  <GuiOption value="key">
-                    Hola
-                  </GuiOption>
-                </GuiSelect>
+                <div className='mb-6'>
+                  <label className="font-medium text-base text-slate-800 mb-1 block">Rol</label>
+                  <GuiSelect
+                    value={role}
+                    placeholder={RoleType[role][0] ?? 'Selecciona un tipo'}
+                    onChange={(e) => setRole(e)}
+                    className="mb-4"
+                  >
+                    {Object.entries(RoleType).map(([key, [label]]) => (
+                      <GuiOption key={key} value={key}>{label}</GuiOption>
+                    ))}
 
-                <SimpleInput
-                  label="Contraseña temporal"
-                  placeholder="Ingresa tu nueva dirección"
-                  value={hash}
-                  className="mb-4"
-                  disabled={true}
-                // value={newAddress}
-                // onChange={(e) => setNewAddress(e.target.value)}
-                />
-                <p className='text-sm text-neutral-600 leading-snug'>Esta debe ser cambiada una vez iniciada la sesión.</p>
+                  </GuiSelect>
+                  <p className='px-1 text-sm text-neutral-600 leading-snug'>{RoleType[role][1] || 's'}</p>
+                </div>
 
+                <div className='mb-6'>
+                  <SimpleInput
+                    label="Contraseña temporal"
+                    placeholder="Ingresa tu nueva dirección"
+                    value={hash}
+                    className="mb-4"
+                    disabled={true}
+                  // value={newAddress}
+                  // onChange={(e) => setNewAddress(e.target.value)}
+                  />
+                  <p className='px-1 text-sm text-neutral-600 leading-snug'>Esta debe ser cambiada una vez iniciada la sesión.</p>
+                </div>
 
               </Dialog>
             </div>
           </div>
 
 
-          <div className="relative">
-            <div className="flex items-center justify-between flex-col md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white">
-              <div className="relative">
-                <button
-                  onClick={toggleDropdown}
-                  className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5"
-                >
-                  <span className="sr-only">Action button</span>
-                  Action
-                  <svg className="w-2.5 h-2.5 ms-2.5" fill="none" viewBox="0 0 10 6">
-                    <path d="M1 1l4 4 4-4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
-                </button>
+          {/* Tabla de empleados */}
+          {staff.length > 0 ? (
+            <div className="relative">
+              <div className="flex items-center justify-between flex-col md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white">
+                <div className="relative">
+                  <button
+                    onClick={toggleDropdown}
+                    className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5"
+                  >
+                    <span className="sr-only">Action button</span>
+                    Action
+                    <svg className="w-2.5 h-2.5 ms-2.5" fill="none" viewBox="0 0 10 6">
+                      <path d="M1 1l4 4 4-4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </button>
 
-                {dropdownOpen && (
-                  <div className="absolute z-10 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">
-                    <ul className="py-1 text-sm text-gray-700">
-                      <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Reward</a></li>
-                      <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Promote</a></li>
-                      <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Activate account</a></li>
-                    </ul>
-                    <div className="py-1">
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Delete User</a>
+                  {dropdownOpen && (
+                    <div className="absolute z-10 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">
+                      <ul className="py-1 text-sm text-gray-700">
+                        <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Reward</a></li>
+                        <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Promote</a></li>
+                        <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Activate account</a></li>
+                      </ul>
+                      <div className="py-1">
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Delete User</a>
+                      </div>
                     </div>
-                  </div>
-                )}
-              </div>
-
-              <div className="relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg className="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 20 20">
-                    <path d="M19 19l-4-4M15 8a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
+                  )}
                 </div>
-                <input
-                  type="text"
-                  id="table-search-users"
-                  className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-0"
-                  placeholder="Search for users"
-                />
-              </div>
-            </div>
 
-            <table className="w-full text-sm text-left text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                  <th className="p-4">
-                    <div className="flex items-center">
-                      {/* Puedes implementar el "select all" si lo deseas */}
-                      <input
-                        id="checkbox-all-search"
-                        type="checkbox"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2"
-                        onChange={(e) => {
-                          const checked = e.target.checked;
-                          setSelectedUsers(checked ? staff.map(u => u.id) : []);
-                        }}
-                        checked={selectedUsers.length === staff.length}
-                      />
-                      <label htmlFor="checkbox-all-search" className="sr-only">Select all</label>
-                    </div>
-                  </th>
-                  <th className="px-6 py-3">Nombre</th>
-                  <th className="px-6 py-3">Función</th>
-                  <th className="px-6 py-3">Status</th>
-                  <th className="px-6 py-3">Acción</th>
-                </tr>
-              </thead>
-              <tbody>
-                {staff.map(user => (
-                  <UserRow
-                    key={user.id}
-                    user={user}
-                    isChecked={isSelected(user.id)}
-                    onToggle={() => handleToggleUser(user.id)}
+                <div className="relative">
+                  <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 20 20">
+                      <path d="M19 19l-4-4M15 8a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </div>
+                  <input
+                    type="text"
+                    id="table-search-users"
+                    className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-0"
+                    placeholder="Search for users"
                   />
-                ))}
-              </tbody>
-            </table>
-          </div>
+                </div>
+              </div>
 
+              <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th className="p-4">
+                      <div className="flex items-center">
+                        {/* Puedes implementar el "select all" si lo deseas */}
+                        <input
+                          id="checkbox-all-search"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2"
+                          onChange={(e) => {
+                            const checked = e.target.checked;
+                            setSelectedUsers(checked ? staff.map(u => u.id) : []);
+                          }}
+                          checked={selectedUsers.length === staff.length}
+                        />
+                        <label htmlFor="checkbox-all-search" className="sr-only">Select all</label>
+                      </div>
+                    </th>
+                    <th className="px-6 py-3">Nombre</th>
+                    <th className="px-6 py-3">Función</th>
+                    <th className="px-6 py-3">Status</th>
+                    <th className="px-6 py-3">Acción</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {staff.length > 0 && staff.map(user => (
+                    <UserRow
+                      key={user.id}
+                      user={user}
+                      isChecked={isSelected(user.id)}
+                      onToggle={() => handleToggleUser(user.id)}
+                    />
+                  ))}
+                </tbody>
+              </table>
 
-
+            </div>
+          ) : (
+            <div className="w-full max-w-md mx-auto flex flex-col justify-center items-center text-neutral-600 h-48 gap-4">
+              <UsersRound className="size-10" />
+              <h2 className='text-2xl font-medium font-app'>Aún no tienes personal</h2>
+              <p>Agrega miembros para comenzar a construir tu equipo.</p>
+            </div>
+          )}
 
         </div>
       </LoadingSection>
@@ -274,7 +323,7 @@ const UserRow = ({ user, isChecked, onToggle }) => (
         <div className="font-normal text-gray-500">{user.email}</div>
       </div>
     </th>
-    <td className="px-6 py-4">{user.role}</td>
+    <td className="px-6 py-4">{RoleType[user.role][0] || "???"}</td>
     <td className="px-6 py-4">
       <div className="flex items-center">
         <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>
